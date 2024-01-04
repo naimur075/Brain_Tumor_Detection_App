d411641a9746a8c558881692cdc47b7ef92a376e
Naimur Hassan <alvihassan61@gmail.com>
2024-01-04 11:55:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 import streamlit as st
-from img_classification import teachable_machine_classification
 from PIL import Image, ImageOps
 from tensorflow.keras.preprocessing.image import load_img,img_to_array
 from tensorflow.keras.models import load_model
 import numpy as np
 import keras
+
+def teachable_machine_classification(img, weights_file):
+    
+    model = keras.models.load_model(weights_file)
+
+    
+    data = np.ndarray(shape=(1, 240, 240, 3), dtype=np.float32)
+    image = img
+  
+    size = (240, 240)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+  
+    image_array = np.asarray(image)
+  
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+  
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    return np.argmax(prediction)
+
 st.title("Brain Tumor or Healthy Brain")
 st.header("Brain Tumor MRI Classifier")
 st.text("Upload a brain MRI Image for image classification as tumor or Healthy Brain")
