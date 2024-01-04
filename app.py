dc93b81dfd78d97db1f9124abe2faa703e059fc7
Naimur Hassan <alvihassan61@gmail.com>
2024-01-04 13:00:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,47 @@
 import streamlit as st
-from PIL import Image, ImageOps
-from tensorflow.keras.preprocessing.image import load_img,img_to_array
-from tensorflow.keras.models import load_model
+from PIL import Image
 import numpy as np
-import keras
+from tensorflow.keras.models import load_model
 
-def teachable_machine_classification(img, weights_file):
-    
-    model = keras.models.load_model(weights_file)
-
-    
-    data = np.ndarray(shape=(1, 240, 240, 3), dtype=np.float32)
-    image = img
-  
+# Function to preprocess and classify the image
+def teachable_machine_classification(img, model):
     size = (240, 240)
-    image = image.resize(size, 'ANTIALIAS')
+    image = img.resize(size, Image.ANTIALIAS)
 
-  
     image_array = np.asarray(image)
-  
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
-  
+    data = np.ndarray(shape=(1, 240, 240, 3), dtype=np.float32)
     data[0] = normalized_image_array
 
     prediction = model.predict(data)
     return np.argmax(prediction)
 
-st.title("Brain Tumor or Healthy Brain")
-st.header("Brain Tumor MRI Classifier")
-st.text("Upload a brain MRI Image for image classification as tumor or Healthy Brain")
-     
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("Classifying...")
-    
-    st.write("")
-    label = teachable_machine_classification(image, 'best_model_f1_0.7660_epoch_99.h5')
-    if label == 0:
-       st.write("The MRI scan detects a brain tumor")
-    else:
-       st.write("The MRI scan shows an healthy brain")
-   
-        
-        
+def main():
+    st.title("Brain Tumor or Healthy Brain")
+    st.header("Brain Tumor MRI Classifier")
+    st.text("Upload a brain MRI Image for image classification as tumor or Healthy Brain")
+
+    # Load the model
+    model_file_path = 'best_model_f1_0.7660_epoch_99.h5'
+    model = load_model(model_file_path)
+
+    # File uploader
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("Classifying...")
+
+        # Classify the image
+        label = teachable_machine_classification(image, model)
+
+        # Display the result
+        if label == 0:
+            st.write("The MRI scan detects a brain tumor")
+        else:
+            st.write("The MRI scan shows a healthy brain")
+
+if __name__ == "__main__":
+    main()
